6a218f0b6a280e5a2bd13d226c0c372e08ab26b3
Paul Robertson <paul.robertson@costain.com>
2021-01-01 18:20:04 +0000

    keep websocket connection alive

--- websockets.js
+++ websockets.js
@@ -22,6 +22,10 @@ exports.connect = function (server) {
       console.log('removing client');
       _.remove(clients, client);
     });
+    // keep the connection alive
+    var id = setInterval(function () {
+      ws.send(JSON.stringify(new Date()));
+    }, 10000);
   });
 };
 
